f983ff0d6c9a265cd8b6517a9a0934030a156c4a
nikena22 <nikenandini411@gmail.com>
2024-07-17 10:04:09 +0700

    Cart component

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-    return view('components.hero-component');
+Route::get('cart', function () {
+    return view('components.cart-component');
 });
