f281987ec015cbedac12944d255d54e1dea95bed
jingga29 <jinggajoshiorlandoii@gmail.com>
2024-07-25 08:08:05 +0700

    update admin panel

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kasir</title>
-</head>
-<body>
-    <h1>KASIR PANEL</h1>
-</body>
-</html>
+@extends('layouts.kasir')
+@section('content')
+<div class="container">
+    <h1>Kasir Dashboard</h1>
+</div>
+@endsection
