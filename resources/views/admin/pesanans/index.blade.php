d0e289270d78fa6b6a831fca7ecb737c495c07fe
jingga29 <jinggajoshiorlandoii@gmail.com>
2024-08-09 10:53:13 +0700

    update pesanans

--- resources/views/admin/pesanans/index.blade.php
+++ resources/views/admin/pesanans/index.blade.php
@@ -5,7 +5,7 @@
     {{-- <h1 class="h3 mb-2 text-gray-800">Daftar Pesanan</h1> --}}
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Pesanan</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Daftar Pesanan</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
