78152968784b81ce772c6caa8a28cc45ef36f585
laelatulkna <laelatulkna2103@gmail.com>
2024-08-28 22:13:04 -0300

    status pesanan

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\MenuController;
 use App\Http\Controllers\PesananController;
 use App\Http\Controllers\KeranjangController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AcountController;
+use App\Http\Controllers\AccountController;
 use App\Models\Menu;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -29,19 +29,19 @@ Route::get('/admin/login', [AuthController::class, 'index'])->name('auth.index')
 Route::post('/admin/auth', [AuthController::class, 'verify'])->name('auth.verify');
 
 Route::middleware('auth:admin')->group(function () {
-
-    Route::get('/admin', function () {
+    
+    Route::get('/admin', function(){
         return view('admin.index');
     })->name('admin');
 
     Route::get('/admin/menu', [MenuController::class, 'index'])->name('list-menu');
-    Route::get('/admin/pesanan', [PesananController::class, 'index'])->name('list-pesanan');
 
     Route::get('/admin/kategori', [KategoriController::class, 'index'])->name('list-kategori');
 
-    Route::get('/admin/menu/tambah', function () {
+    Route::get('/admin/menu/tambah', function(){
         return view('admin.menu.tambah');
     })->name('menuTambah');
+    
 });
 
 // DATA
@@ -49,22 +49,23 @@ Route::get('list-kategoris', [KategoriController::class, 'kategori'])->name('dat
 Route::get('list-menu', [MenuController::class, 'menu'])->name('data.menu');
 
 Route::middleware('auth:kasir')->group(function () {
-
-    Route::get('/kasir', function () {
+    
+    Route::get('/kasir', function(){
         return view('kasir.index');
     })->name('kasir');
 
-    Route::get('/kasir/menu', function () {
+    Route::get('/kasir/menu', function(){
         return view('kasir.menu.index');
     })->name('menumenuKasir');
 
-    Route::get('/kasir/menu/tambah', function () {
+    Route::get('/kasir/menu/tambah', function(){
         return view('kasir.menu.tambah');
     })->name('menuTambahKasir');
+
 });
 
-Route::post('/user/auth', [AuthController::class, 'checkAuth'])->name('check.auth');
-Route::post('/user/auth-success', [AuthController::class, 'ajaxLoginWithName'])->name('user.login');
+Route::post('/user/auth', [AuthController::class,'checkAuth'])->name('check.auth');
+Route::post('/user/auth-success', [AuthController::class,'ajaxLoginWithName'])->name('user.login');
 
 Route::get('/', function () {
     return view('components.hero-component');
@@ -78,31 +79,49 @@ Route::get('menu', function () {
     return view('components.menu-component', compact('data'));
 })->name('menu');
 
-Route::middleware('auth:admin')->group(function () {
-    Route::get('/admin/kategori', [KategoriController::class, 'index'])->name('list-kategori');
+
+
+Route::get('cart', function () {
+    return view('components.cart-component');
+})->middleware('name.auth')->name('cart');
+
+    //Menu
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('/admin/menu', [MenuController::class, 'index'])->name('list-menu');
+        Route::get('/admin/menu/{id}/edit', [MenuController::class, 'edit'])->name('edit-menu');
+        Route::post('/admin/menu', [MenuController::class, 'store'])->name('store-menu');
+        Route::put('/admin/menu/{id}', [MenuController::class, 'update'])->name('update-menu');
+        Route::delete('/admin/menu/{id}', [MenuController::class, 'destroy'])->name('delete-menu');
+    });
+
+    // Kategori
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('/admin/kategori', [KategoriController::class, 'index'])->name('list-kategori');
+        Route::get('list-kategoris', [KategoriController::class, 'kategori'])->name('data.kategori');
+        Route::post('/admin/kategori', [KategoriController::class, 'store'])->name('kategori.store');
+        Route::get('/admin/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
+        Route::put('/admin/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+        Route::delete('/admin/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+    });
+    
+    // Checkout
+    Route::post('/checkout', [PesananController::class, 'checkout'])->name('pesanan.checkout');
+    Route::get('/checkout/success', [PesananController::class, 'checkoutSuccess'])->name('checkout.success');
+    
+    // Keranjang
+    Route::get('cart', [KeranjangController::class, 'index'])
+        ->middleware('name.auth')
+        ->name('cart');
+    Route::post('/cart', [KeranjangController::class, 'addToCart'])->name('add.cart');
+    
     Route::get('list-pesanans', [PesananController::class, 'pesanan'])->name('data.pesanan');
-    Route::get('list-kategoris', [KategoriController::class, 'kategori'])->name('data.kategori');
-    Route::post('/admin/kategori', [KategoriController::class, 'store'])->name('kategori.store');
-    Route::get('/admin/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
-    Route::put('/admin/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-    Route::delete('/admin/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
-});
 
-Route::middleware('auth:admin')->group(function () {
-    Route::get('/admin/menu', [MenuController::class, 'index'])->name('list-menu');
-    Route::get('/admin/menu/{id}/edit', [MenuController::class, 'edit'])->name('edit-menu');
-    Route::post('/admin/menu', [MenuController::class, 'store'])->name('store-menu');
-    Route::put('/admin/menu/{id}', [MenuController::class, 'update'])->name('update-menu');
-    Route::delete('/admin/menu/{id}', [MenuController::class, 'destroy'])->name('delete-menu');
-});
+    // Daftar Pesanan
+    Route::get('/admin/pesanan', [PesananController::class, 'index'])->name('list-pesanan');
 
+    // Management User
+    Route::get('/admin/management-user', [UserController::class, 'index'])->name('data.user');
 
-Route::post('/checkout', [PesananController::class, 'checkout'])->name('pesanan.checkout');
-Route::get('/checkout/success', [PesananController::class, 'checkoutSuccess'])->name('checkout.success');
+    Route::post('/admin/pesanan/{id}/update-status', [PesananController::class, 'updateStatus']);
 
-Route::get('cart', [KeranjangController::class, 'index'])
-    ->middleware('name.auth')
-    ->name('cart');
-Route::post('/cart', [KeranjangController::class, 'addToCart'])->name('add.cart');
-
-Route::get('/admin/management-user', [UserController::class, 'index'])->name('data.user');
+    Route::delete('/admin/pesanan/{id}/delete', [PesananController::class, 'delete']);
