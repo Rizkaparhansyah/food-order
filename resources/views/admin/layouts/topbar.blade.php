5f91abf51cc5d7408e146b6ca1e7cb0d208bb63b
nikena22 <nikenandini411@gmail.com>
2024-09-30 10:21:33 +0700

    all

--- resources/views/admin/layouts/topbar.blade.php
+++ resources/views/admin/layouts/topbar.blade.php
@@ -40,7 +40,7 @@
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
-                <img class="img-profile rounded-circle" src="{{asset('IMG/Rizka Parhansyah.jpg')}}">
+                <img class="img-profile rounded-circle" src=">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
