16dc52c12d24cbc26e72afd37a0b672e76128f03
Rizka Parhansyah <rizka.parhan@gmail.com>
2024-10-17 10:11:37 +0700

    up layout

--- resources/views/admin/layouts/topbar.blade.php
+++ resources/views/admin/layouts/topbar.blade.php
@@ -40,9 +40,8 @@
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
-                <img class="img-profile rounded-circle" src=">
+                <img class="img-profile rounded-circle" src="">
             </a>
-            <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">
